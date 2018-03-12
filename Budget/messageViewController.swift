fdc756ee71a2acec3444592f9af2642a651eaac2
Benny Nguyen <benny.nguyenx@gmail.com>
2018-03-12 02:40:17 -0700

    Made ability to open messages app

--- Budget/messageViewController.swift
+++ Budget/messageViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 import MessageUI
 
-class messageViewController: UIViewController {
-
+class messageViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    guard let messageAppURL = NSURL(string: "sms:")
-      else { return }
-    if UIApplication.shared.canOpenURL(messageAppURL as URL) {
-      UIApplication.shared.openURL(messageAppURL as URL)
+    
+  }
+
+  // Send a message about the budget if "send" is pressed
+  @IBAction func pressSend(_ sender: UIButton) {
+    let controller = MFMessageComposeViewController()
+    if (MFMessageComposeViewController.canSendText()) {
+      controller.body = "Here's my budget if you're interested: "
+      controller.messageComposeDelegate = self
+      self.present(controller, animated: true, completion: nil)
     }
   }
+  
+  // Conform to MFMessageComposeViewControllerDelegate protocol
+  func messageComposeViewController(_ controller: MFMessageComposeViewController,
+                                    didFinishWith result: MessageComposeResult) {
+    controller.dismiss(animated: true, completion: nil)}
 }
